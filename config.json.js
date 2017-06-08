af7524509265fff9ee43eef9af6789ea6af2a853
Kenney <zql254@vip.qq.com>
2017-06-08 09:23:34 +0800

    change ico

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
-    "build_date": "2017-06-07 15:13:48", 
+    "build_date": "2017-06-08 09:23:24", 
     "description": "Kcptun加速", 
     "home_url": "Module_kcptun.asp", 
-    "md5": "3c75d69a94a6cd10dafc94c297e562fe", 
+    "md5": "83138e17835f5575a40f8928b88c5035", 
     "module": "kcptun", 
     "name": "kcptun", 
     "tar_url": "kcptun/kcptun.tar.gz", 
